81d16a344bf6662fc52eb827072c4b5f377d4872
Arapov Vasyl <panda.dagon@gmail.com>
2022-05-21 23:01:21 +0300

    BtnSort

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import PostItem from './components/PostItem';
 import PostList from './components/PostList';
 import MyButton from './components/UI/button/MyButton';
 import MyInput from './components/UI/input/MyInput';
+import MySelect from './components/UI/select/MySelect';
 import './styles/App.css';
 function App() {
   const [posts, setPosts] = useState([
@@ -12,7 +13,7 @@ function App() {
     { id: 2, title: 'Javascript 2', body: 'Description' },
     { id: 3, title: 'Javascript 3', body: 'Description' },
   ])
-
+const [selectedSort,setSelecedSort]=useState('');
   const createPost=(newPost)=>{
 setPosts([...posts,newPost])
   }
@@ -20,17 +21,26 @@ setPosts([...posts,newPost])
   const removePost=(post)=>{
     setPosts(posts.filter(p=>p.id !== post.id))
   }
+
+  const sortPosts=(sort)=>{
+    selectedSort(sort);
+  }
   
   return (
     <div className="App">
       <PostForm create={createPost}/>
       <hr style={{margin:"15px 0"}}/>
       <div>
-        <select>
-          <option value='valuel'>By name</option>
-          <option value='valuel'>By description</option>
+        <MySelect
+        value={selectedSort}
+        onChange={sortPosts}
+        defaultValue="Sort by"
+        options={[
+          {value:'title',name:'By name'},
+          {value:'body',name:'By description'}
 
-        </select>
+        ]}
+        />
       </div>
       {posts.length !==0
       ?<PostList remove={removePost} posts={posts} title={'List Posts: 1'} />
--- src/components/UI/select/MySelect.jsx
+++ src/components/UI/select/MySelect.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 
-const MySelect = ({options,defaultValue}) => {
+const MySelect = ({options,defaultValue,value,onChange}) => {
     return (
-        <select>
-<option value="">{defaultValue}</option>
+        <select 
+        value={value}
+        onChange={event=>onChange(event.target.value)}
+        >
+<option disabled value="">{defaultValue}</option>
 {options.map(option=>
-    <option value={option.value}>
+    <option key={option.value} value={option.value}>
         {option.name}
     </option>)
 }
